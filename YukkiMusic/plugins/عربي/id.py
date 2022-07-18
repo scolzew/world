ea0e5cc5b2907f5b5d865463aa45d58e42dfa873
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 00:10:08 +0200

    Update id.py

--- YukkiMusic/plugins/عربي/id.py
+++ YukkiMusic/plugins/عربي/id.py
@@ -17,7 +17,7 @@ async def khalid(client: Client, message: Message):
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""◂ اسمك ↫ {message.from_user.mention}\n\n◂ معرفك ↫ @{message.from_user.username}\n\n◂ ايديك ↫ {message.from_user.id}\n\n◂ ايدي الجروب ↫ {message.chat.id}""", 
+                    await message.reply_photo(photo.file_id,       caption=f"""⇐𓂄𓆩💕نـيـمـك𓆪𓂁 {message.from_user.mention}\n\n⇐𓂄𓆩💕يوزرك𓆪𓂁 @{message.from_user.username}\n\n⇐𓂄𓆩💕الايدي بتاعك𓆪𓂁 {message.from_user.id}\n\n⇐𓂄𓆩💕ايدي الجروب𓆪𓂁 {message.chat.id}""", 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
