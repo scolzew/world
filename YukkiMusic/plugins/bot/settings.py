84c15e577c67f9c62322f5e4017ba3fc14ec3269
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 20:26:06 +0200

    Update settings.py

--- YukkiMusic/plugins/bot/settings.py
+++ YukkiMusic/plugins/bot/settings.py
@@ -99,8 +99,8 @@ async def settings_back_markup(
             OWNER = None
         buttons = private_panel(_, app.username, OWNER)
         return await CallbackQuery.edit_message_photo(
-          photo=config.START_IMG_URL,
-          caption=_["start_2"].format(MUSIC_BOTOWNER),
+            photo=config.START_IMG_URL,
+            caption=_["start_2"].format(MUSIC_BOTOWNER),
             reply_markup=InlineKeyboardMarkup(buttons),
         )
     else:
