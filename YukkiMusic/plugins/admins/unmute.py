fd1a690053cc5adb885e719103961b5dd5d44917
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 06:03:56 +0200

    Add files via upload

--- YukkiMusic/plugins/admins/unmute.py
+++ YukkiMusic/plugins/admins/unmute.py
@@ -8,18 +8,45 @@
 # All rights reserved.
 
 from pyrogram import filters
-from pyrogram.types import Message
-
-from config import BANNED_USERS
+from pyrogram import filters
+from YukkiMusic.utils.database import is_music_playing, music_off
 from strings import get_command
+from strings.filters import command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_muted, mute_off
+from YukkiMusic.utils.database import set_loop
 from YukkiMusic.utils.decorators import AdminRightsCheck
-
+from YukkiMusic.utils.database import is_muted, mute_on
+from YukkiMusic.utils.database import is_muted, mute_off
+from YukkiMusic.utils.database import is_music_playing, music_on
+from datetime import datetime
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL, lyrical
+from YukkiMusic.utils import bot_sys_stats
+from YukkiMusic.utils.decorators.language import language
+import random
+import re
+import string
+import lyricsgenius as lg
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
+                            
 # Commands
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
+MUTE_COMMAND = get_command("MUTE_COMMAND")
+STOP_COMMAND = get_command("STOP_COMMAND")
+RESUME_COMMAND = get_command("RESUME_COMMAND")
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
+PING_COMMAND = get_command("PING_COMMAND")
+LYRICS_COMMAND = get_command("LYRICS_COMMAND")
 
+api_key = "Vd9FvPMOKWfsKJNG9RbZnItaTNIRFzVyyXFdrGHONVsGqHcHBoj3AI3sIlNuqzuf0ZNG8uLcF9wAd5DXBBnUzA"
+y = lg.Genius(
+    api_key,
+    skip_non_songs=True,
+    excluded_terms=["(Remix)", "(Live)"],
+    remove_section_headers=True,
+)
+y.verbose = False
 
 @app.on_message(
     filters.command(UNMUTE_COMMAND)
@@ -37,4 +64,127 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     await Yukki.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    filters.command(MUTE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def mute_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if await is_muted(chat_id):
+        return await message.reply_text(_["admin_5"])
+    await mute_on(chat_id)
+    await Yukki.mute_stream(chat_id)
+    await message.reply_text(
+        _["admin_6"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    filters.command(STOP_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Yukki.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_9"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    filters.command(RESUME_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    await Yukki.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    filters.command(PAUSE_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Yukki.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    filters.command(LYRICS_COMMAND) & ~filters.edited & ~BANNED_USERS
+)
+@language
+async def lrsearch(client, message: Message, _):
+    if len(message.command) < 2:
+        return await message.reply_text(_["lyrics_1"])
+    title = message.text.split(None, 1)[1]
+    m = await message.reply_text(_["lyrics_2"])
+    S = y.search_song(title, get_full_info=False)
+    if S is None:
+        return await m.edit(_["lyrics_3"].format(title))
+    ran_hash = "".join(
+        random.choices(string.ascii_uppercase + string.digits, k=10)
+    )
+    lyric = S.lyrics
+    if "Embed" in lyric:
+        lyric = re.sub(r"\d*Embed", "", lyric)
+    lyrical[ran_hash] = lyric
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["L_B_1"],
+                    url=f"https://t.me/{app.username}?start=lyrics_{ran_hash}",
+                ),
+            ]
+        ]
+    )
+    await m.edit(_["lyrics_4"], reply_markup=upl)
+
+@app.on_message(
+    filters.command(PING_COMMAND)
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await Yukki.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
+        )
     )
