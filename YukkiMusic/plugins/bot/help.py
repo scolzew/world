71cd650ad5da50637a12794a1412a045c5477a3f
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 16:18:25 +0200

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -71,8 +71,9 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_text(_["help_1"], reply_markup=keyboard)
-
+        await update.reply_photo(
+        photo=config.START_IMG_URL,
+        caption=_["help_1"], reply_markup=keyboard)
 
 @app.on_message(
     filters.command(HELP_COMMAND)
