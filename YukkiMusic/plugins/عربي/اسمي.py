152c1f70ecb2771a30b8ca1710b2938de5fbf533
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 15:52:13 +0200

    Update اسمي.py

--- YukkiMusic/plugins/عربي/اسمي.py
+++ YukkiMusic/plugins/عربي/اسمي.py
@@ -158,7 +158,7 @@ def echo(client, msg):
 async def khalid(client: Client, message: Message):
     usr = await client.get_users(message.from_user.about)
     name = usr.first_name
-    async for photo in client.iter_profile_phmessage.from_user.about, limit=1):
+    async for photo in client.iter_profile_photos(message.from_user.about, limit=1):
                     await message.reply_text( 
                     f"""الـبـايـو💕 ⇐ {message.from_user.about}""", 
         reply_markup=InlineKeyboardMarkup(
