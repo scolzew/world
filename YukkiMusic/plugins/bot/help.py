dd1ad497ab65267d97e0261f5a3970137cb31583
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 16:21:52 +0200

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -52,7 +52,7 @@ async def helper_private(
         keyboard = help_pannel(_, True)
         if update.message.photo:
             await update.message.delete()
-             await update.message.reply_photo(
+             await message.reply_photo(
               photo=config.START_IMG_URL,
               caption=_["help_1"], reply_markup=keyboard
             )
