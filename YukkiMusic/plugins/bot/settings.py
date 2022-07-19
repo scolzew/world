c9c3075f3f970f069eb8d7ef6781c43973b5033b
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-19 19:53:08 +0200

    Update settings.py

--- YukkiMusic/plugins/bot/settings.py
+++ YukkiMusic/plugins/bot/settings.py
@@ -98,8 +98,9 @@ async def settings_back_markup(
         except:
             OWNER = None
         buttons = private_panel(_, app.username, OWNER)
-        return await CallbackQuery.edit_message_text(
-            _["start_2"].format(MUSIC_BOT_NAME, OWNER),
+        return await CallbackQuery.edit_messphoto(
+          photo=config.START_IMG_URL,
+          caption=_["start_2"].format(MUSIC_BOT_NAME, message.from_user.mention),
             reply_markup=InlineKeyboardMarkup(buttons),
         )
     else:
