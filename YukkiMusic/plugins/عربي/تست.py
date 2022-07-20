91dbc455da6aa33146b6bbb9051228798d2d94bc
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-20 18:01:28 +0200

    Add files via upload

--- YukkiMusic/plugins/عربي/تست.py
+++ YukkiMusic/plugins/عربي/تست.py
@@ -407,7 +407,7 @@ async def play_commnd(
                     _,
                     track_id,
                     user_id,
-                    "v" if video else "a",
+                    "ف" if video else "a",
                     "c" if channel else "g",
                     "f" if fplay else "d",
                 )
@@ -559,7 +559,7 @@ async def play_music(client, CallbackQuery, _):
             _["play_15"],
             reply_markup=InlineKeyboardMarkup(buttons),
         )
-    video = True if mode == "v" else None
+    video = True if mode == "ف" else None
     ffplay = True if fplay == "f" else None
     try:
         await stream(
@@ -636,7 +636,7 @@ async def play_playlists_command(client, CallbackQuery, _):
         _["play_2"].format(channel) if channel else _["play_1"]
     )
     videoid = lyrical.get(videoid)
-    video = True if mode == "v" else None
+    video = True if mode == "ف" else None
     ffplay = True if fplay == "f" else None
     spotify = True
     if ptype == "yt":
