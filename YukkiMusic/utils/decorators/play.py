4e2b9807c4bc8494a5a94ee46016f7a20f80077c
jsksjssjnssjdkd <101276719+jsksjssjnssjdkd@users.noreply.github.com>
2022-07-20 18:11:20 +0200

    Update play.py

--- YukkiMusic/utils/decorators/play.py
+++ YukkiMusic/utils/decorators/play.py
@@ -109,13 +109,13 @@ def PlayWrapper(command):
                 else:
                     if message.from_user.id not in admins:
                         return await message.reply_text(_["play_4"])
-        if message.command[0][0] == "v":
+        if message.command[0][0] == "ف":
             video = True
         else:
             if "-v" in message.text:
                 video = True
             else:
-                video = True if message.command[0][1] == "v" else None
+                video = True if message.command[0][1] == "ف" else None
         if message.command[0][-1] == "e":
             if not await is_active_chat(chat_id):
                 return await message.reply_text(_["play_18"])
