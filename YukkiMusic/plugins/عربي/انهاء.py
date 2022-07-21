9a35f2ff0e1127655fb63cc4299a3842feebb4d0
scolzew <108234178+scolzew@users.noreply.github.com>
2022-07-21 15:29:48 +0200

    Add files via upload

---
+++ YukkiMusic/plugins/عربي/انهاء.py
@@ -0,0 +1,393 @@
+from YukkiMusic.utils.database import is_music_playing, music_off
+from strings import get_command
+import asyncio
+from strings.filters import command
+from YukkiMusic import app
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.utils.database import set_loop
+from YukkiMusic.utils.decorators import AdminRightsCheck
+from YukkiMusic.utils.database import is_muted, mute_on
+from YukkiMusic.utils.database import is_muted, mute_off
+from YukkiMusic.utils.database import is_music_playing, music_on
+from datetime import datetime
+from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL, lyrical, START_IMG_URL, MONGO_DB_URI, OWNER_ID
+from YukkiMusic.utils import bot_sys_stats
+from YukkiMusic.utils.decorators.language import language
+import random
+import config
+import re
+from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+import string
+import lyricsgenius as lg
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
+from pyrogram import Client, filters
+from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from typing import Union
+import sys
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_USERNAME = getenv("BOT_USERNAME")
+
+START_IMG_URL = getenv("START_IMG_URL")
+
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
+
+# Commands
+STOP_COMMAND = get_command("STOP_COMMAND")
+PAUSE_COMMAND = get_command("PAUSE_COMMAND")
+MUTE_COMMAND = get_command("MUTE_COMMAND")
+UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
+RESUME_COMMAND = get_command("RESUME_COMMAND")
+PING_COMMAND = get_command("PING_COMMAND")
+LYRICS_COMMAND = get_command("LYRICS_COMMAND")
+
+api_key = "Vd9FvPMOKWfsKJNG9RbZnItaTNIRFzVyyXFdrGHONVsGqHcHBoj3AI3sIlNuqzuf0ZNG8uLcF9wAd5DXBBnUzA"
+y = lg.Genius(
+    api_key,
+    skip_non_songs=True,
+    excluded_terms=["(Remix)", "(Live)"],
+    remove_section_headers=True,
+)
+y.verbose = False
+
+
+@app.on_message(
+    command(["اسكت","انهاء"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def stop_music(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    await Yukki.stop_stream(chat_id)
+    await set_loop(chat_id, 0)
+    await message.reply_text(
+        _["admin_35"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    command(["ايقاف","توقف"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def pause_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if not await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_1"])
+    await music_off(chat_id)
+    await Yukki.pause_stream(chat_id)
+    await message.reply_text(
+        _["admin_2"].format(message.from_user.mention)
+    )
+    
+@app.on_message(
+    command(["ميوت"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def mute_admin(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if await is_muted(chat_id):
+        return await message.reply_text(_["admin_5"])
+    await mute_on(chat_id)
+    await Yukki.mute_stream(chat_id)
+    await message.reply_text(
+        _["admin_6"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    command(["فك ميوت"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def unmute_admin(Client, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if not await is_muted(chat_id):
+        return await message.reply_text(_["admin_7"])
+    await mute_off(chat_id)
+    await Yukki.unmute_stream(chat_id)
+    await message.reply_text(
+        _["admin_8"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    command(["استكمال","استئناف","استكمل","كمل"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@AdminRightsCheck
+async def resume_com(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1:
+        return await message.reply_text(_["general_2"])
+    if await is_music_playing(chat_id):
+        return await message.reply_text(_["admin_3"])
+    await music_on(chat_id)
+    await Yukki.resume_stream(chat_id)
+    await message.reply_text(
+        _["admin_4"].format(message.from_user.mention)
+    )
+
+@app.on_message(
+    command(["بنج","البنج"])
+    & filters.group
+    & ~filters.edited
+    & ~BANNED_USERS
+)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"],
+    )
+    start = datetime.now()
+    pytgping = await Yukki.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            MUSIC_BOT_NAME, resp, UP, DISK, CPU, RAM, pytgping
+        )
+    )
+    
+@app.on_message(
+    command(["كلمات"]) & ~filters.edited & ~BANNED_USERS
+)
+@language
+async def lrsearch(client, message: Message, _):
+    if len(message.command) < 2:
+        return await message.reply_text(_["lyrics_1"])
+    title = message.text.split(None, 1)[1]
+    m = await message.reply_text(_["lyrics_2"])
+    S = y.search_song(title, get_full_info=False)
+    if S is None:
+        return await m.edit(_["lyrics_3"].format(title))
+    ran_hash = "".join(
+        random.choices(string.ascii_uppercase + string.digits, k=10)
+    )
+    lyric = S.lyrics
+    if "Embed" in lyric:
+        lyric = re.sub(r"\d*Embed", "", lyric)
+    lyrical[ran_hash] = lyric
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["L_B_1"],
+                    url=f"https://t.me/{app.username}?start=lyrics_{ran_hash}",
+                ),
+            ]
+        ]
+    )
+    await m.edit(_["lyrics_4"], reply_markup=upl)
+
+@app.on_message(
+    command(["اسمي"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_text( 
+                    f"""ٲسـمـك💕 ⇐ {message.from_user.mention}""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+
+@app.on_message(
+    command(["صورتي"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""غير صورتك بقا قرفتنا 😏""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{message.from_user.username}")
+                ],[
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞.╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+    
+@app.on_message(
+    command(["معرفي"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_text( 
+                    f"""مـعـرفـك💕 ⇐ @{message.from_user.username}""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+    
+@app.on_message(
+    command(["بوت"])
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"{START_IMG_URL}",
+        caption=f"اسمي {MUSIC_BOT_NAME} ينجم 😮‍💨💕", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+
+@app.on_message(
+    filters.command(["id"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""نـيـمـك💕 ⇐{message.from_user.mention}\n\nيـوزرك💕 ⇐ @{message.from_user.username}\n\nالايـدي بـتـاعـك💕 ⇐ {message.from_user.id}\n\nايـدي الـجـروب💕 ⇐ {message.chat.id}""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+
+@app.on_message(
+    command(["ايدي","الايدي"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""نـيـمـك💕 ⇐{message.from_user.mention}\n\nيـوزرك💕 ⇐ @{message.from_user.username}\n\nالايـدي بـتـاعـك💕 ⇐ {message.from_user.id}\n\nايـدي الـجـروب💕 ⇐ {message.chat.id}""", 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+    
+@app.on_message(
+    command(["قول"])
+    & filters.group
+    & ~filters.edited
+)
+def echo(client, msg):
+    text = msg.text.split(None, 1)[1]
+    msg.reply(text)
+
+@app.on_message(
+    command(["انا مين"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    usr = await client.get_users(message.from_user.id)
+    name = usr.first_name
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    await message.reply_text( 
+                    f"""💘 ¦ انت روحي » """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ],
+            ]
+        ),
+    )
+                    
+@app.on_message(
+     command(["مبرمج السورس","المطور","مطور السورس","المبرمج"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/a6c96cdbd066ca2388d06.jpg",
+        caption=f"""◍ مش محتاجين نكتب كلام كتير خش ع اول زرار وانت هتعرف""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                InlineKeyboardButton("• 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍 ☤ ", url=f"https://t.me/WORLD_MUSIC_F"),
+                ],[
+                InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+                ]
+            ]
+        ),
+    )
+
+@app.on_message(
+    command(["سورس","السورس"])
+    & filters.group
+    & ~filters.edited
+)
+async def khalid(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/a6c96cdbd066ca2388d06.jpg",
+        caption=f"""[ٓ❍ | 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙎𝙊𝙐𝙍𝘾𝙀 𝙒𝙊𝙍𝙇𝘿 .](https://t.me/ch_world_music)\n\n[❍ | 𝙒𝙊𝙍𝙇𝘿 𝙏𝙃𝙀 𝘽𝙀𝙎𝙏 𝙎𝙊𝙐𝙍𝘾𝙀 𝙊𝙉 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈 .](https://t.me/ch_world_music)\n\n[❍ | 𝙁𝘼𝙎𝙏 𝙎𝙊𝙐𝙍𝘾𝙀 𝙄𝙉 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈.](https://t.me/ch_world_music)\n\n[❍ | 𝘿𝙀𝙑𝙀𝙇𝙊𝙋𝙀𝙍 𝙎𝙊𝙐𝙍𝘾𝙀 ](https://t.me/WORLD_MUSIC_F)""",
+        reply_markup=InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                        "╞. 𝐒𝐨𝐮𝐫𝐜𝐞 .╡", url=f"https://t.me/ch_world_music"),
+            ],[
+                InlineKeyboardButton("✚ Add me to your Group", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+            ]
+        ]
+         ),
+     )
+  
